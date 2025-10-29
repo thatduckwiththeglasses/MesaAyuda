fb730cc6374f30daf89c22287f6643dac86d5baf
Juan Patricio Michel <60205729+thatduckwiththeglasses@users.noreply.github.com>
2025-10-29 00:50:38 -0300

    correccion de direccion en listarTicket

--- HTML/listarTicket.js
+++ HTML/listarTicket.js
@@ -28,7 +28,7 @@ console.log("contacto:"+query.contacto);
 console.log("ultima_fecha:"+query.fecha_ultimo_ingreso);
 console.log("mode:"+query.mode);
 
-document.getElementById("lastlogin").innerHTML = "<table><tr><td>ID del Cliente</td><td>"+query.id+"</td></tr><tr><td>Nombre</td><td>"+query.nombre+"</td></tr><tr><td>Contacto</td><td>"+query.contacto+"</td></tr><tr><td>Ultimo ingreso</td><td>"+query.fecha_ultimo_ingreso+"</td></tr></table>";
+document.getElementById("lastlogin").innerHTML = "<table><tr><td>Cliente</td><td>"+query.id+"</td></tr><tr><td>Nombre</td><td>"+query.nombre+"</td></tr><tr><td>Contacto</td><td>"+query.contacto+"</td></tr><tr><td>Ultimo ingreso</td><td>"+query.fecha_ultimo_ingreso+"</td></tr></table>";
 
 const systemURL={ 
 
